0c6101f0e0557e4b8dd53cb35ae62cd77c00f57f
jeffrey82221 <jeffrey82221@gmail.com>
2022-11-14 00:11:38 +0800

    feature: add some properties to data class

--- json_schema_generation_for_web_crawler/overview_pypi_json.py
+++ json_schema_generation_for_web_crawler/overview_pypi_json.py
@@ -1,3 +1,21 @@
+"""
+TODO:
+    - [ ] Release Version
+        - [ ] Version class with version id as input
+        - [ ] develop sorting algorithm for version ids
+        - [ ] Version constraint class that takes dependent string as input
+        - [ ] Version constraint class that can determine whether a Version obj fit or not
+    - [ ] Platform
+        - [ ] Develop platform tags data cleaning strategy at PypiVersionPackageVeiw
+            - [ ] seperate by ; and ,
+            - [ ] lower cases
+            - [ ] strip space
+            - [ ] allow decomposition into different os
+        - [ ] Building Platform node object
+    - [ ] Develop python-version property (returning Python-version objs)
+    - [ ] Build Dependency-Hyperedge Obj
+"""
+import typing
 from common.view_json import SingleView, OverView
 
 class PyPiPackageView(SingleView):
@@ -33,33 +51,52 @@ class PypiVersionPackageView(SingleView):
         self._pkg = pkg
         self._version = version
         super().__init__(schema=None)
+
     @property
     def url(self):
         return f'https://pypi.org/pypi/{self._pkg}/{self._version}/json'
 
+    @property
+    def author_info(self):
+        author = self.methods[('info', 'author')]()
+        email = self.methods[('info', 'author_email')]()
+        return (author, email)
+
+    @property
+    def maintainer_info(self):
+        maintainer = self.methods[('info', 'maintainer')]()
+        email = self.methods[('info', 'maintainer_email')]()
+        return (maintainer, email)
+
+
+
 class PypiPackageAllVersionView(OverView):
     def __init__(self, pkg):
-        pkg_view = PyPiPackageView(pkg)
-        inputs = [(pkg, version) for version in pkg_view.released_versions]
+        self._pkg_view = PyPiPackageView(pkg)
+        inputs = [(pkg, version) for version in self.versions]
         super().__init__(PypiVersionPackageView, inputs)
 
+    @property
+    def versions(self) -> typing.List[str]:
+        """
+        Get sorted version ids
+        """
+        return list(self._pkg_view.released_versions)
+
+    @property
+    def releases(self) -> PypiVersionPackageView:
+        """
+        Get sorted release data objs
+        """
+        pass
+
 if __name__ == '__main__':
-    vo = PypiPackageAllVersionView('tensorflow')
-    print(vo.get('info', 'author_email'), vo.get('info', 'maintainer_email'))
-    """
-    v = PypiVersionPackageView('pandas', '1.5.0rc0')
-    print(v.methods.keys())
-    s = PyPiPackageView('pytz')
-    print('========================== pandas ================================')
-    print(s.methods.keys())
-    print(s.methods[('info', 'author')]())
-    print(s.released_versions)
-    v = PypiVersionPackageView('pandas', '1.5.0rc0')
-    print(s.methods.keys())
-    print(s.methods[('info', 'requires_dist')]())
-    print('done')
-    
-    """
+    ver = PypiPackageAllVersionView('pandas').versions[0]
+    print(ver)
+    p = PypiVersionPackageView('pandas', ver)
+    print(p.author_info)
+    print(p.maintainer_info)
+
     """
     print('========================== overview =============================')
     
@@ -68,13 +105,14 @@ if __name__ == '__main__':
         for i, pkg in enumerate(f):
             pkg = pkg.strip()
             pkgs.append(pkg)
-    o = PyPiPackageOverView(pkgs[:10000])
-    
-    print(o.method_keys)
-    with open('requires_python.txt', 'w') as f:
-        for re_py in o.get('info', 'requires_python'):
+    o = PyPiPackageOverView(pkgs[:1000])
+
+    with open('platform.txt', 'w') as f:
+        for re_py in o.get('info', 'platform'):
             if re_py is not None:
                 f.write(re_py.strip()+'\n')
+
+
     import json
     with open('released_versions.json', 'w') as f:
         json.dump(o.released_versions, f)
