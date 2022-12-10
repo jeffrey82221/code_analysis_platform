b43bfb3ed69aa1ec4b780f793a41fc9b702e9b2f
jeffrey82221 <jeffrey82221@gmail.com>
2022-12-10 12:12:35 +0800

    feature: add pypy schema inference experiment (2.3X speed up)

---
+++ json_schema_inference/overview_by_pypy.py
@@ -0,0 +1,19 @@
+"""
+PyPy speed up experiment on schema inferencing
+Result: 2.3 X speed up. 
+
+How to execute in pypy: 
+`pypy3 overview_by_pypy.py`
+"""
+from common.inference_base import InferenceEngine
+from common.schema_objs import Union
+import json
+import tqdm
+jsonl_path = 'data/kaggle_data/test.jsonl'
+total = sum(1 for _ in open(jsonl_path))
+with open(jsonl_path) as f:
+    json_pipe = map(json.loads, f)
+    json_pipe = tqdm.tqdm(json_pipe, total = total)
+    schema = InferenceEngine.get_schema(json_pipe)
+
+print(schema)
