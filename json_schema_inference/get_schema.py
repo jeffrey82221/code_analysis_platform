59ae1159d54589492bd9fc0ce52bc9eda2b6464b
jeffrey82221 <jeffrey82221@gmail.com>
2022-12-11 22:06:21 +0800

    feature: generate so files

--- json_schema_inference/get_schema.py
+++ json_schema_inference/get_schema.py
@@ -1,3 +1,11 @@
+"""
+Compile this module: 
+
+```
+python -m nuitka --module --include-package=common get_schema.py
+```
+"""
+
 from common.schema import InferenceEngine
 
 def get_schema(json_batch):
